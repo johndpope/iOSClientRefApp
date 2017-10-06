3c6bfaa559dabb1f9d633c071f2078ef09f844e3
Fredrik Sjöberg <fredrik.sjoberg@oneagency.se>
2017-10-06 23:05:49 +0200

    EMP-10445 Expanded restore functionality for downloads on session restore

--- iOSReferenceApp/AppDelegate.swift
+++ iOSReferenceApp/AppDelegate.swift
@@ -81,7 +81,18 @@ extension AppDelegate {
     func application(_ application: UIApplication, handleEventsForBackgroundURLSession identifier: String, completionHandler: @escaping () -> Void) {
         if identifier == SessionManager.defaultSessionConfigurationIdentifier {
             print("🛏 Rejoining session \(identifier)")
-            SessionManager.default.backgroundCompletionHandler = completionHandler
+            let sessionManager = SessionManager.default
+            sessionManager.backgroundCompletionHandler = completionHandler
+            
+            sessionManager.restore(assigningRequesterFor: { assetId -> DownloadFairplayRequester? in
+                // TODO: Create an asset specific requester (for example from Exposure
+                return nil
+            }) { downloadTasks in
+                downloadTasks.forEach {
+                    // Restore state
+                    $0.resume()
+                }
+            }
         }
     }
     
