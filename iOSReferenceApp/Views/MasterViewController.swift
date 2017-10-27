64db73b282f4f2c01784de0896794ae26703ea68
Fredrik Sjöberg <fredrik.sjoberg@oneagency.se>
2017-10-28 01:08:18 +0200

    Show+hide menu with animation

--- iOSReferenceApp/Views/MasterViewController.swift
+++ iOSReferenceApp/Views/MasterViewController.swift
@@ -31,46 +31,6 @@ class MasterViewController: UIViewController {
         }
     }
     
-    fileprivate let menuConstants = MenuConstants()
-    @IBOutlet weak var leadingContentConstraint: NSLayoutConstraint!
-    @IBOutlet weak var trailingContentConstraint: NSLayoutConstraint!
-    @IBOutlet weak var leadingBlurConstraint: NSLayoutConstraint!
-    @IBOutlet weak var trailingBlurConstraint: NSLayoutConstraint!
-    @IBOutlet weak var blurView: UIVisualEffectView!
-    
-    fileprivate var menuController: MainMenuViewController!
-    
-    var config: ApplicationConfig?
-    var dynamicCustomerConfig: DynamicCustomerConfig?
-    var environment: Environment!
-    var sessionToken: SessionToken!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-//        applyDynamicConfigUI()
-        blurView.effect = nil
-        
-        guard let env = UserInfo.environment else { return }
-        config = ApplicationConfig(environment: env)
-        config?.setup { [weak self] in
-//            self?.loadDynamicConfig()
-        }
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(true, animated: false)
-        navigationItem.hidesBackButton = true
-        
-        menuController.constrain(width: menuConstants.inset(for: view.bounds.size.width))
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
 //    func loadDynamicConfig() {
 //        config?.fetchFile(fileName: "main.json") { [weak self] file in
 //            if let jsonData = file?.config {
@@ -103,52 +63,151 @@ class MasterViewController: UIViewController {
 //            .processor(CrispResizingImageProcessor(referenceSize: logoSize, mode: .aspectFit))
 //        ]
 //    }
+//
+
+    fileprivate let menuConstants = MenuConstants()
     
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    @IBOutlet weak var contentContainer: UIView!
+    
+    fileprivate var menuController: MainMenuViewController!
+    var animator: UIDynamicAnimator!
+    var itemBehavior: UIDynamicItemBehavior!
+    var snapBehavior: UISnapBehavior!
+    var  attachmentBehavior: UIAttachmentBehavior!
+    
+    var config: ApplicationConfig?
+    var dynamicCustomerConfig: DynamicCustomerConfig?
+    var environment: Environment!
+    var sessionToken: SessionToken!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        animator = UIDynamicAnimator(referenceView: view)
+        itemBehavior = UIDynamicItemBehavior(items: [contentContainer])
+        itemBehavior.allowsRotation = false
+        blurView.effect = nil
+        
+        
+        
+//        applyDynamicConfigUI()
+        
+        guard let env = UserInfo.environment else { return }
+        config = ApplicationConfig(environment: env)
+        config?.setup { [weak self] in
+//            self?.loadDynamicConfig()
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        navigationItem.hidesBackButton = true
+        
+        menuController.constrain(width: menuConstants.inset(for: view.bounds.size.width))
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     @IBAction func blurViewTapAction(_ sender: UITapGestureRecognizer) {
         toggleSlidingMenu()
     }
     
     @IBAction func blurViewSwipeAction(_ sender: UIPanGestureRecognizer) {
-        print(sender.translation(in: view))
-        moveSlidingMenu(offset: sender.translation(in: view))
+        let location = sender.translation(in: view)
+        switch sender.state {
+        case .began:
+            let anchor = CGPoint(x: snapBehavior.snapPoint.x, y: view.bounds.midY)
+            let vector = CGVector(dx: 1, dy: 0)
+            attachmentBehavior = UIAttachmentBehavior.slidingAttachment(with: contentContainer, attachmentAnchor: anchor, axisOfTranslation: vector)
+            attachmentBehavior.attachmentRange = UIFloatRange(minimum: 0, maximum: 100)
+            
+            animator.addBehavior(attachmentBehavior)
+            
+        case .ended:
+            let maxOffset = menuConstants.inset(for: view.bounds.size.width)
+            print(maxOffset+location.x,maxOffset, 0.5*maxOffset)
+            if maxOffset+location.x < 0.75*maxOffset {
+                // Hide menu
+                animator.removeBehavior(attachmentBehavior)
+                menuOpen = false
+                blurView.isUserInteractionEnabled = false
+                
+                
+                let x = contentContainer.bounds.midX
+                let y = contentContainer.frame.midY
+                
+                animator.addBehavior(snapBehavior)
+                snapBehavior.snapPoint = CGPoint(x: x, y: y)
+                UIView.animate(withDuration: 0.2, delay: 0, options: .curveEaseIn, animations: { [weak self] in
+                    self?.blurView.effect = nil
+                })
+            }
+            else {
+                // Keep menu
+                let x = contentContainer.bounds.midX + menuConstants.inset(for: view.bounds.size.width)
+                let y = contentContainer.frame.midY
+                
+                animator.removeBehavior(attachmentBehavior)
+                
+                animator.addBehavior(snapBehavior)
+                snapBehavior.snapPoint = CGPoint(x: x, y: y)
+            }
+        default:
+            attachmentBehavior.anchorPoint = CGPoint(x: snapBehavior.snapPoint.x+location.x, y: view.bounds.midY)
+        }
     }
     
     enum Segue: String {
         case masterToMainMenu = "masterToMainMenu"
         case masterToContent = "masterToContent"
     }
+    
+    var menuOpen: Bool = false
 }
 
 extension MasterViewController: SlidingMenuController {
-    private var menuIsOpen: Bool {
-        return leadingContentConstraint.constant != menuConstants.defaultInset
-    }
-    
     func moveSlidingMenu(offset: CGFloat) {
-        let delta = leadingContentConstraint.constant + offset
+        print(offset)
     }
     
     func toggleSlidingMenu() {
-        blurView.isUserInteractionEnabled = !menuIsOpen
-        
-        leadingContentConstraint.constant = menuIsOpen ? menuConstants.defaultInset : menuConstants.inset(for: view.bounds.size.width)
-        trailingContentConstraint.constant = menuIsOpen ? menuConstants.defaultInset : -menuConstants.inset(for: view.bounds.size.width)
-        
-        
-        leadingBlurConstraint.constant = !menuIsOpen ? menuConstants.defaultInset : menuConstants.inset(for: view.bounds.size.width)
-        
-        trailingBlurConstraint.constant = !menuIsOpen ? menuConstants.defaultInset : -menuConstants.inset(for: view.bounds.size.width)
-        
-        let menuEndsOpen = menuIsOpen
-        UIView.animate(withDuration: 0.2, delay: 0, options: .curveEaseIn, animations: { [weak self] in
-            if menuEndsOpen {
-                self?.blurView.effect = UIBlurEffect(style: .dark)
-            }
-            else {
+        if menuOpen {
+            menuOpen = false
+            blurView.isUserInteractionEnabled = false
+
+            print("toggle back")
+            let x = contentContainer.frame.midX - menuConstants.inset(for: view.bounds.size.width)
+            let y = contentContainer.frame.midY
+            
+            print("SnapToEND",CGPoint(x: x, y: y))
+            snapBehavior.snapPoint = CGPoint(x: x, y: y)
+            UIView.animate(withDuration: 0.2, delay: 0, options: .curveEaseIn, animations: { [weak self] in
                 self?.blurView.effect = nil
-            }
-            self?.view.layoutIfNeeded()
-        })
+            })
+        }
+        else {
+            menuOpen = true
+            blurView.isUserInteractionEnabled = true
+            animator.removeAllBehaviors()
+
+            let x = contentContainer.frame.midX + menuConstants.inset(for: view.bounds.size.width)
+            let y = contentContainer.frame.midY
+
+            print("SnapTo",CGPoint(x: x, y: y))
+            snapBehavior = UISnapBehavior(item: contentContainer, snapTo: CGPoint(x: x, y: y))
+            snapBehavior.damping = 0.75
+            animator.addBehavior(snapBehavior)
+
+            UIView.animate(withDuration: 0.2, delay: 0, options: .curveEaseIn, animations: { [weak self] in
+                self?.blurView.effect = UIBlurEffect(style: .dark)
+            })
+
+        }
     }
 }
 
