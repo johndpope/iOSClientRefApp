29405e5bb4376c6e670bdb7176f9c6f54707fe83
Fredrik Sjöberg <fredrik.sjoberg@oneagency.se>
2017-10-28 22:06:41 +0200

    My downloads and asset details project structure

--- iOSReferenceApp/AssetDetails/AssetDetailsViewController.swift
+++ iOSReferenceApp/AssetDetails/AssetDetailsViewController.swift
@@ -13,52 +13,6 @@ import Player
 import AVKit
 import Download
 
-protocol AssetDetailsPresenter: class, AuthorizedEnvironment {
-    var assetDetailsPresenter: UIViewController { get }
-}
-
-extension AssetDetailsPresenter {
-    func presetDetails(for asset: Asset, from origin: AssetDetailsViewController.PresentedFrom) {
-        guard let assetType = asset.type else {
-            assetDetailsPresenter.showMessage(title: "AssetType missing", message: "Please check Exposure response")
-            return
-        }
-        
-        switch assetType {
-        case .tvChannel: presentEPG(for: asset)
-        default: presentVod(for: asset, from: origin)
-        }
-    }
-    
-    fileprivate func presentVod(for asset: Asset, from origin: AssetDetailsViewController.PresentedFrom) {
-        let uiStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        if let vc = uiStoryboard.instantiateViewController(withIdentifier: "AssetDetailsViewController") as? AssetDetailsViewController {
-            vc.bind(viewModel: AssetDetailsViewModel(asset: asset,
-                                                     environment: environment,
-                                                     sessionToken: sessionToken))
-            vc.bind(downloadViewModel: DownloadAssetViewModel(environment: environment,
-                                                              sessionToken: sessionToken))
-            vc.presentedFrom = origin
-            assetDetailsPresenter.present(vc, animated: true) { }
-        }
-    }
-    
-    fileprivate func presentEPG(for asset: Asset) {
-        guard asset.assetId != nil else {
-            assetDetailsPresenter.showMessage(title: "ChannelId missing", message: "Please check Exposure response")
-            return
-        }
-        
-        let uiStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        if let vc = uiStoryboard.instantiateViewController(withIdentifier: "EPGDetailsViewController") as? EPGDetailsViewController {
-            vc.bind(viewModel: EPGDetailsViewModel(channelAsset: asset,
-                                                   environment: environment,
-                                                   sessionToken: sessionToken))
-            assetDetailsPresenter.present(vc, animated: true) { }
-        }
-    }
-}
-
 class AssetDetailsViewController: UIViewController {
     
     @IBOutlet weak var scrollView: LazyScrollView!
