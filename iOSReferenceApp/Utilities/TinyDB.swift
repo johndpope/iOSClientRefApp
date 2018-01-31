1591d6b0b14f2e287596ac603178938a9984049b
Fredrik Sjöberg <fredrik.sjoberg@webstep.se>
2018-01-31 13:24:35 +0100

    Remove redundant dependencies

---
+++ iOSReferenceApp/Utilities/TinyDB.swift
@@ -0,0 +1,21 @@
+//
+//  TinyDB.swift
+//  iOSReferenceApp
+//
+//  Created by Fredrik Sjöberg on 2018-01-31.
+//  Copyright © 2018 emp. All rights reserved.
+//
+
+import Foundation
+
+//class TinyDB {
+//    static func save(string: String, for key: String) {
+//        UserDefaults.standard.set(string, forKey: key)
+//        UserDefaults.standard.synchronize()
+//    }
+//    
+//    static func string(for key: String) -> String? {
+//        return UserDefaults.standard.string(forKey: key)
+//    }
+//}
+
